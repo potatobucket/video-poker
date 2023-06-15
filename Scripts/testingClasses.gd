bb32a5890add48aaf65dacddb1d151e5f2571c09
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-06-15 08:35:51 -0500

    Small Tweaks 2
    All the commented-out test code should now be deleted.

--- Scripts/testingClasses.gd
+++ Scripts/testingClasses.gd
@@ -16,11 +16,7 @@ var deck = TestDeck
 func _ready():
 	objectDeck = deck.deck
 	draw_hand()
-#	print(objectDeck)
 
 func draw_hand():
 	for c in range(0, maxHandSize):
 		hand.append(deck.deck[c])
-#		print(hand[c].cardName)
-#		print(handCard.cardValue, " ", handCard.cardSuit, " ", objectDeck.size())
-#		await get_tree().create_timer(0.5).timeout

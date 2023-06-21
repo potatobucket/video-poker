84f987d3cd3f95101b2359e166e1b52edaa42216
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-06-21 15:30:42 -0500

    Setting up some debug stuff
    Sorting hands (sort of works) and trying as hard as I can to think about how to check which hands are winners.

--- Scripts/debug_hands.gd
+++ Scripts/debug_hands.gd
@@ -17,14 +17,23 @@ var debugHandTwoPair = []
 
 func _ready():
 	deck.create_deck()
-#	generate_royal_flush()
-#	generate_straight_flush()
-#	generate_four_of_a_kind()
-#	generate_full_house()
-#	generate_peasant_flush()
-#	generate_straight()
-#	generate_three_of_a_kind()
-#	generate_two_pair()
+	generate_royal_flush()
+	generate_straight_flush()
+	generate_four_of_a_kind()
+	generate_full_house()
+	generate_peasant_flush()
+	generate_straight()
+	generate_three_of_a_kind()
+	generate_two_pair()
+	print(debugHandRoyalFlush)
+	print(debugHandStraightFlush)
+	print(debugHandFourOfAKind)
+	print(debugHandFullHouse)
+	print(debugHandPeasantFlush)
+	print(debugHandRegularStraight)
+	print(debugHandThreeOfAKind)
+	print(debugHandTwoPair)
+	pass
 
 func generate_royal_flush():
 	debugHandRoyalFlush.append(deck.deck[0])
@@ -32,8 +41,10 @@ func generate_royal_flush():
 	debugHandRoyalFlush.append(deck.deck[10])
 	debugHandRoyalFlush.append(deck.deck[11])
 	debugHandRoyalFlush.append(deck.deck[12])
-	for a in debugHandRoyalFlush.size():
-		print(debugHandRoyalFlush[a])
+#	debugHandRoyalFlush.sort_custom(debug_sort_by_suit_and_then_value)
+	debugHandRoyalFlush.sort_custom(debug_sort_by_value_and_then_suit)
+#	for a in debugHandRoyalFlush.size():
+#		print(debugHandRoyalFlush[a])
 
 func generate_straight_flush():
 	debugHandStraightFlush.append(deck.deck[15])
@@ -41,8 +52,10 @@ func generate_straight_flush():
 	debugHandStraightFlush.append(deck.deck[17])
 	debugHandStraightFlush.append(deck.deck[18])
 	debugHandStraightFlush.append(deck.deck[19])
-	for b in debugHandStraightFlush.size():
-		print(debugHandStraightFlush[b])
+#	debugHandStraightFlush.sort_custom(debug_sort_by_suit_and_then_value)
+	debugHandStraightFlush.sort_custom(debug_sort_by_value_and_then_suit)
+#	for b in debugHandStraightFlush.size():
+#		print(debugHandStraightFlush[b])
 
 func generate_four_of_a_kind():
 	debugHandFourOfAKind.append(deck.deck[7])
@@ -50,8 +63,10 @@ func generate_four_of_a_kind():
 	debugHandFourOfAKind.append(deck.deck[33])
 	debugHandFourOfAKind.append(deck.deck[46])
 	debugHandFourOfAKind.append(deck.deck[39])
-	for c in debugHandStraightFlush.size():
-		print(debugHandStraightFlush[c])
+#	debugHandFourOfAKind.sort_custom(debug_sort_by_suit_and_then_value)
+	debugHandFourOfAKind.sort_custom(debug_sort_by_value_and_then_suit)
+#	for c in debugHandStraightFlush.size():
+#		print(debugHandStraightFlush[c])
 
 func generate_full_house():
 	debugHandFullHouse.append(deck.deck[0])
@@ -59,8 +74,10 @@ func generate_full_house():
 	debugHandFullHouse.append(deck.deck[26])
 	debugHandFullHouse.append(deck.deck[12])
 	debugHandFullHouse.append(deck.deck[25])
-	for d in debugHandFullHouse.size():
-		print(debugHandFullHouse[d])
+#	debugHandFullHouse.sort_custom(debug_sort_by_suit_and_then_value)
+	debugHandFullHouse.sort_custom(debug_sort_by_value_and_then_suit)
+#	for d in debugHandFullHouse.size():
+#		print(debugHandFullHouse[d])
 
 func generate_peasant_flush():
 	debugHandPeasantFlush.append(deck.deck[9])
@@ -68,8 +85,10 @@ func generate_peasant_flush():
 	debugHandPeasantFlush.append(deck.deck[8])
 	debugHandPeasantFlush.append(deck.deck[7])
 	debugHandPeasantFlush.append(deck.deck[11])
-	for e in debugHandPeasantFlush.size():
-		print(debugHandPeasantFlush[e])
+#	debugHandPeasantFlush.sort_custom(debug_sort_by_suit_and_then_value)
+	debugHandPeasantFlush.sort_custom(debug_sort_by_value_and_then_suit)
+#	for e in debugHandPeasantFlush.size():
+#		print(debugHandPeasantFlush[e])
 
 func generate_straight():
 	debugHandRegularStraight.append(deck.deck[1])
@@ -77,8 +96,10 @@ func generate_straight():
 	debugHandRegularStraight.append(deck.deck[29])
 	debugHandRegularStraight.append(deck.deck[43])
 	debugHandRegularStraight.append(deck.deck[44])
-	for f in debugHandRegularStraight.size():
-		print(debugHandRegularStraight[f])
+#	debugHandRegularStraight.sort_custom(debug_sort_by_suit_and_then_value)
+	debugHandRegularStraight.sort_custom(debug_sort_by_value_and_then_suit)
+#	for f in debugHandRegularStraight.size():
+#		print(debugHandRegularStraight[f])
 
 func generate_three_of_a_kind():
 	debugHandThreeOfAKind.append(deck.deck[0])
@@ -86,8 +107,10 @@ func generate_three_of_a_kind():
 	debugHandThreeOfAKind.append(deck.deck[26])
 	debugHandThreeOfAKind.append(deck.deck[43])
 	debugHandThreeOfAKind.append(deck.deck[44])
-	for g in debugHandThreeOfAKind.size():
-		print(debugHandThreeOfAKind[g])
+#	debugHandThreeOfAKind.sort_custom(debug_sort_by_suit_and_then_value)
+	debugHandThreeOfAKind.sort_custom(debug_sort_by_value_and_then_suit)
+#	for g in debugHandThreeOfAKind.size():
+#		print(debugHandThreeOfAKind[g])
 
 func generate_two_pair():
 	debugHandTwoPair.append(deck.deck[3])
@@ -95,5 +118,23 @@ func generate_two_pair():
 	debugHandTwoPair.append(deck.deck[34])
 	debugHandTwoPair.append(deck.deck[47])
 	debugHandTwoPair.append(deck.deck[5])
-	for h in debugHandTwoPair.size():
-		print(debugHandTwoPair[h])
+#	debugHandTwoPair.sort_custom(debug_sort_by_suit_and_then_value)
+	debugHandTwoPair.sort_custom(debug_sort_by_value_and_then_suit)
+#	for h in debugHandTwoPair.size():
+#		print(debugHandTwoPair[h])
+
+func debug_sort_by_suit_and_then_value(a, b):
+	if a[1] == b[1]:
+		if deck.values[a[0]] < deck.values[b[0]]:
+			return true
+	elif deck.values[a[0]] > deck.values[b[0]]:
+		return true
+	return false
+
+func debug_sort_by_value_and_then_suit(a, b):
+	if deck.values[a[0]] == deck.values[b[0]]:
+		if a[1] < b[1]:
+			return true
+	elif a[1] > b[1]:
+		return true
+	return false
